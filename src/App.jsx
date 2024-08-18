c7ebe993bd99e51fa5dcbbd4c4d1fec491fb9283
h8D3jCa2b <kapiladentalgroup@gmail.com>
2024-08-18 15:39:41 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Count is {count}
         </button>
       </div>
       <p className="read-the-docs">
